13e7982f61354058c1e89f74b99aab01c4de2e99
linezas <41704004+linezas@users.noreply.github.com>
2018-12-01 03:52:35 +0200

    Add files via upload

---
+++ asmens sveikatos ugdymas/Testas 'Organizmo funkcionalumas fizinio krūvio metu'_files/yui_combo.php
@@ -0,0 +1,2 @@
+
+// Combo resource m/1525439694/theme_bcu/zoom/zoom-min.js ($CFG->dirroot/theme/bcu/yui/zoom/zoom.js) not found!
---
+++ asmens sveikatos ugdymas/Testas 'Organizmo funkcionalumas fizinio krūvio metu'_files/yui_combo_004.php
@@ -0,0 +1,2 @@
+
+// Combo resource m/1525439694/theme_bcu/full/full-min.js ($CFG->dirroot/theme/bcu/yui/full/full.js) not found!
